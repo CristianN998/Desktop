70abd1c05dc3c50cb441304f0be9ef620f954131
Cristian Casallas Developer <cristiancasallas998@gmail.com>
2021-12-29 14:41:10 -0500

    añado proyecto terminado de tarjeta personal con html y css

---
+++ front-end-developer/mycard.js
@@ -0,0 +1,10 @@
+button00 = document.getElementById("caja00");
+button01 = document.getElementById("caja01");
+button02 = document.getElementById("caja02");
+button03 = document.getElementById("caja03");
+
+button00.onclick("abrirTwitter");
+
+function abrirTwitter() {
+    window.open("https://www.w3schools.com");
+}
