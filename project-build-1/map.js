08363df50aa36cf23c3cd895a41eb6b46fe6a71a
Cristian Casallas Developer <cristiancasallas998@gmail.com>
2022-03-07 11:35:31 -0500

    added fully responsive and javascript debugged

--- project-build-1/map.js
+++ project-build-1/map.js
@@ -1,5 +1,5 @@
 function initializeMap() {
-    var coord = { lat: 4.5783879, lng: -74.1551567 };
+    var coord = { lat: 4.57937, lng: -74.2190087 };
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
         center: coord,
--- project-build-1/script.js
+++ project-build-1/script.js
@@ -7,7 +7,7 @@ function nextPage() {
     document.getElementById("number").innerHTML = "02";
     document.getElementById("title").innerHTML = "INTERIORISMO";
     document.getElementById("text").innerHTML =
-        "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks.";
+        "te ayudamos a realizar tus ideas y proyectos, sin excusas, sin trabas, puntuales y siempre atento a lo que tu necesitas";
     const square01 = document.getElementById("change-image-01");
     const square02 = document.getElementById("change-image-02");
     const square03 = document.getElementById("change-image-03");
@@ -37,7 +37,7 @@ function firstPage() {
     document.getElementById("number").innerHTML = "01";
     document.getElementById("title").innerHTML = "CONSTRUCCIÓN";
     document.getElementById("text").innerHTML =
-        " Normally, both your asses would be dead as fucking fried chicken,but you happen to pull this shit while I'm in a transitional period";
+        "   Enchapes, mamposteria, estucados, techos pvc, ormamentación, estructuras, plomeria y más, ofrecemos calidad al precio adecuado";
     const square01 = document.getElementById("change-image-01");
     const square02 = document.getElementById("change-image-02");
     const square03 = document.getElementById("change-image-03");
@@ -84,11 +84,16 @@ function removeActiveClasses() {
 
 function changeBg() {
     const images = [
-        "url('https://i.ibb.co/BPBrYCP/1.jpg')",
-        "url('https://i.ibb.co/0ZZ9wrH/gallery-card-01-mobile-02.jpg')",
-        "url('https://i.ibb.co/KsHHLmp/gallery-card-01-mobile-03.jpg')",
-        "url('https://i.ibb.co/w6j7SXP/gallery-card-01-mobile-04.jpg')",
-        "url('https://i.ibb.co/JytsMPX/gallery-card-01-mobile-05.jpg')",
+        "url('https://i.ibb.co/Sv7CPrf/galeria-images-00.jpg')",
+        "url('https://i.ibb.co/0XvvBLp/galeria-images-01.jpg')",
+        "url('https://i.ibb.co/H4YJXMb/galeria-images-02.jpg')",
+        "url('https://i.ibb.co/Rptp0N3/galeria-images-03.jpg')",
+        "url('https://i.ibb.co/PrqH9Sh/galeria-images-04.jpg')",
+        "url('https://i.ibb.co/3kLcX9W/galeria-images-05.jpg')",
+        "url('https://i.ibb.co/F04VXy8/galeria-images-06.jpg')",
+        "url('https://i.ibb.co/yYGT2PB/galeria-images-07.jpg')",
+        "url('https://i.ibb.co/x8H1kH5/galeria-images-08.jpg')",
+        "url('https://i.ibb.co/cJQKn89/galeria-images-09.jpg')",
     ];
     const container = document.querySelector(".card.active");
     const bg = images[Math.floor(Math.random() * images.length)];
