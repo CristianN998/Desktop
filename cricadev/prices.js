be12da0bd4082f5df4c6b4f1e417775a7d40885f
Cristian Casallas Developer <cristiancasallas998@gmail.com>
2022-03-15 10:43:18 -0500

    list of prices of our cricadev project

---
+++ cricadev/prices.js
@@ -0,0 +1,9 @@
+const pricesCricadev2022 = {
+    type: ["BasicLandingPage"],
+    pages: ["3 to 5"],
+    price: ["90-140 USD"],
+};
+const pricesSteasdev2022 = {
+    type: ["Logo"],
+    price: ["10 to 20 USD"],
+};
